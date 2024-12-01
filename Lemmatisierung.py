4cd21a8a8df6e2c5267d1282bcf8490293008422
Reptile128 <lolotron766@gmail.com>
2024-12-01 18:36:24 +0100

    Löschung

--- Lemmatisierung.py
+++
@@ -1,5 +0,0 @@
-import spacy
-
-
-spcy = spacy.load("de_core_news_sm")
-
--- Tester.py
+++ Tester.py
@@ -1,6 +1,5 @@
 import LinkDetection
 import Read
-import Uebersetzung
 
 liste = Read.lesen("C:\\Users\\tomli\Downloads\\tar.csv")
 
@@ -12,12 +11,3 @@ liste = LinkDetection.LinkEntfernung(liste)
 
 for i in range(1000):
     print(liste[i][1])
-
-#sprache = Uebersetzung.spracherkennung(liste)
-
-#liste = Uebersetzung.ueberstzung(liste, sprache)
-
-#print("\t")
-
-#for i in range(len(liste)):
-  #print(liste[i])
